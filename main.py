9e4919a9e870377547b8c60d73adfd0dad008b1e
Xtalhisto <xuehaoyyds@outlook.com>
2026-03-30 22:20:20 +0800

    修改逻辑使正文抬头能显示不同收件人

--- main.py
+++ main.py
@@ -24,7 +24,9 @@ pre_date = snapshoter.get_previous_snapshot_date(curr_date)
 diff = snapshoter.get_top_growth_repos_safe(current_date=curr_date, previous_date=pre_date)
 for item in diff['results']:
     item['summary'] = analysier.get_response(item)
-html_content = builder.build_html(diff['results']) 
-# with open("test.html", "w", encoding="utf-8") as f:
-#     f.write(html_content)
-emailer.email_send(html_content)
+for people in cfg.recipient:
+    html_content = builder.build_html(diff['results'], people['name'] ) 
+    with open(f"{people['name']}_test.html", "w", encoding="utf-8") as f:
+        f.write(html_content)
+    emailer.email_send(html_content)
+# emailer.email_send_file('test.html')
