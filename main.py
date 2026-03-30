d41c009c142450e22c206cdc2411dc5f4a7c13e5
Xtalhisto <xuehaoyyds@outlook.com>
2026-03-30 16:43:15 +0800

    实现多人接收邮件，cid插入图片路径问题待解决

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.html_maker import Email_Builder
 from utils.snapshot import Repo_Snapshot
 from utils.summarizer import Client_Analysis
 from utils.fetcher import Github_Fetcher
+from utils.emailer import Emailer
 
 
 
@@ -12,11 +13,18 @@ fetcher = Github_Fetcher(cfg)
 snapshoter = Repo_Snapshot(cfg)
 analysier = Client_Analysis(cfg)
 builder = Email_Builder(cfg)
+emailer = Emailer(cfg)
+
+repos = fetcher.fetch_candidate_repos()
+snapshoter.save_snapshot(repos)
+ltst_date = snapshoter.get_latest_snapshot_date()
+all_date = snapshoter.get_all_snapshot_dates()
 curr_date  = snapshoter.get_latest_snapshot_date()
 pre_date = snapshoter.get_previous_snapshot_date(curr_date)
 diff = snapshoter.get_top_growth_repos_safe(current_date=curr_date, previous_date=pre_date)
 for item in diff['results']:
     item['summary'] = analysier.get_response(item)
 html_content = builder.build_html(diff['results']) 
-with open("test.html", "w", encoding="utf-8") as f:
-    f.write(html_content)
+# with open("test.html", "w", encoding="utf-8") as f:
+#     f.write(html_content)
+emailer.email_send(html_content)
