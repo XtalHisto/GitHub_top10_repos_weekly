cf8097b690fd36a6e190e8c50c1607f1aa6c255f
Xtalhisto <xuehaoyyds@outlook.com>
2026-04-01 00:12:39 +0800

    修改逻辑，实现动态计算不同天数的star数增量，而不只局限于一周

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ ltst_date = snapshoter.get_latest_snapshot_date()
 all_date = snapshoter.get_all_snapshot_dates()
 curr_date  = snapshoter.get_latest_snapshot_date()
 pre_date = snapshoter.get_previous_snapshot_date(curr_date)
-diff = snapshoter.get_top_growth_repos_safe(current_date=curr_date, previous_date=pre_date)
-for item in diff['results']:
-    item['summary'] = analysier.get_response(item)
+diffs = snapshoter.get_top_growth_repos_safe(current_date=curr_date, previous_date=pre_date)
+for diff in diffs['results']:
+    diff['summary'] = analysier.get_response(diff, diffs['time_delta'])
 for people in cfg.recipient:
-    html_content = builder.build_html(diff['results'], people['name'] ) 
+    html_content = builder.build_html(diffs['results'], people['name'] ) 
     with open(f"{people['name']}_test.html", "w", encoding="utf-8") as f:
         f.write(html_content)
     emailer.email_send(html_content, people['email_address'])
